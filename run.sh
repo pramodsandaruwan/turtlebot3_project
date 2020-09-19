994e02aa649f7d91430a31c65f40b2c918b4b6b8
Pramod Sandaruwan <pramodsandaruwan80@gmail.com>
2020-09-19 20:12:23 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-echo "Turtlebot simulation"
-#source $HOME/pramod/turtlebot_ws/devil/setup.bash
-export TURTLEBOT3_MODEL=waffle_pi
-roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$HOME/map1.yaml
