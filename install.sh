dd99d9a5d654450e97c7ced6a4e25843dface6a3
Pramod Sandaruwan <pramodsandaruwan80@gmail.com>
2020-09-20 16:38:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,8 +26,8 @@ sudo apt update
 rosdep update
 
 # Environment setup - optional. Do not run if multiple versions of ROS are present.
-source /opt/ros/melodic/setup.bash
-echo "source /opt/ros/melodic/setup.bash" >> $HOME/.bashrc
+source /opt/ros/kinetic/setup.bash
+echo "source /opt/ros/kinetic/setup.bash" >> $HOME/.bashrc
 source ~/.bashrc
 
 echo "${green}Creating workspace ...${reset}"
