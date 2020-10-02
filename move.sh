74eb0a224ed370882a8947968adbd38b7764a7c8
pramod sandaruwan <pramodsandaruwan80@gmail.com>
2020-10-02 22:26:15 +0530

    add source data

--- move.sh
+++ move.sh
@@ -1,4 +1,9 @@
 green=`tput setaf 2`
 reset=`tput sgr0`
 echo "${green}Navigating...${reset}"
+
+WS_PATH=$HOME/turtlebot3_ws
+
+source $WS_PATH/devel/setup.bash
+
 rosrun turtlebot3_navigation navigator.py
